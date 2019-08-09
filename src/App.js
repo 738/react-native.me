38e4f66c74bdc1a56cd5fa001db507d38eacae57
Jon Jee <jydrogen@gmail.com>
2019-08-09 15:33:54 +0200

    add meta tag

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <h1>공식 홈페이지</h1>
+      <h1>React Native 리액트 네이티브</h1>
+      <h1 style={{display: 'none'}}>리액트 네이티브 공부 / RN 공부 / 리액트 네이티브 강좌 / 리액트 네이티브 강의 / </h1>
+      <h2>공식 홈페이지</h2>
       <ul>
         <li>
           <a href='https://facebook.github.io/react-native/' target='_blank'>react-native</a>
         </li>
+        <li>
+          <a href='https://github.com/facebook/react-native' target='_blank'>github</a>
+        </li>
       </ul>
-      <h1>Curated List</h1>
+      <h2>Curated List</h2>
       <ul>
         <li>
           <a href='https://www.native.directory/' target='_blank'>native.directory</a>
@@ -22,7 +27,7 @@ function App() {
           <a href='https://js.coach/?collection=React+Native' target='_blank'>js.coach</a>
         </li>
       </ul>
-      <h1>좋은 참고자료</h1>
+      <h2>좋은 참고자료</h2>
       <ul>
         <li>
           <a href='https://drive.google.com/file/d/1LKtyaDE9qnrqOeJvQRhIhWqdZ229ofR7/view?fbclid=IwAR3CXaz0HevOfkHG4DsAoWeh-a67EAAfQaVBZrxl7RNla3uG-sI3J0ggTkQ' target='_blank'>React Native로 성공적인 MVP 개발하기</a>
